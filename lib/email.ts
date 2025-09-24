49f1963674b41a237d4244d09ee8144a1adf6093
Aybala Esmer <aybala@Aybalas-MacBook-Pro.local>
2025-09-23 23:31:47 -0500

    changes

--- lib/email.ts
+++ lib/email.ts
@@ -1,5 +1,6 @@
-import nodemailer from "nodemailer";
 import "server-only";
+import nodemailer from "nodemailer";
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT || 587),
